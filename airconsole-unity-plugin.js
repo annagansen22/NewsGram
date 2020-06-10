d85b330d2b12af59ea7902f5cf66580e95127648
Gansen, A. (Anna) <a.gansen@student.ru.nl>
2020-06-11 01:16:47 +0200

    source game changes

--- airconsole-unity-plugin.js
+++ airconsole-unity-plugin.js
@@ -7,15 +7,13 @@
  * Sets up the communication to the screen.
  */
 
-function App(container, canvas, web_config, progress_config) {
+function App(container, web_config, progress_config) {
     var me = this;
-
     me.is_native_app = typeof Unity != "undefined";
     me.is_editor = !!me.getURLParameterByName("unity-editor-websocket-port");
     me.top_bar_height = window.outerHeight - window.innerHeight;
     me.is_unity_ready = false;
     me.queue = false;
-    me.game_container = container;
     me.web_config = web_config || {};
     me.web_config.width = me.web_config.width || 16;
     me.web_config.height = me.web_config.height || 9;
@@ -25,27 +23,17 @@ function App(container, canvas, web_config, progress_config) {
 
     } else {
         me.initAirConsole();
-
         if (!me.is_native_app) {
-            var progress_bar = null;
-
             if (progress_config) {
-                progress_bar = me.initProgressBar(progress_config, container);
+                me.web_config.onProgress = function(game, progress) {
+                    me.updateProgress(progress_config, game, progress);
+                };
             }
             me.setupErrorHandler();
-
-            createUnityInstance(canvas, me.web_config, function(progress) {
-                if (!progress_config) {
-                    return;
-                }
-
-                me.updateProgressBar(progress_bar, progress);
-            }).then(function(unityInstance) {
-                me.game = unityInstance;
-            }).catch(function(error) {
-                console.error(error);
-            });
-
+            me.game = UnityLoader.instantiate(container, "Build/game.json", me.web_config);
+            if (progress_config) {
+                me.updateProgress(progress_config, me.game, 0);
+            }
             me.resizeCanvas();
         } else {
             me.startNativeApp();
@@ -53,39 +41,29 @@ function App(container, canvas, web_config, progress_config) {
     }
 };
 
-App.prototype.initProgressBar = function(progress_config, game_container) {
-    var bar = document.createElement("div");
-    var fill = document.createElement("div");
-
-    bar.style.position = "absolute";
-    bar.style.left = progress_config.left;
-    bar.style.top = progress_config.top;
-    bar.style.width = progress_config.width;
-    bar.style.height = progress_config.height;
-    bar.style.background = progress_config.background;
-
-    fill.style.width = "0";
-    fill.style.height = "100%";
-    fill.style.top = "0";
-    fill.style.left = "0";
-    fill.style.background = progress_config.color;
-
-    bar.appendChild(fill);
-    game_container.appendChild(bar);
-
-    return {
-        bar: bar,
-        fill: fill,
-    };
-};
-
-App.prototype.updateProgressBar = function(progress_bar, progress) {
-    progress_bar.fill.style.width = progress * 100 + "%";
-
+App.prototype.updateProgress = function(progress_config, game, progress) {
+    if (!game.progress) {
+        container = game.container;
+        var bar = document.createElement("div");
+        bar.style.position = "absolute";
+        bar.style.left = progress_config.left;
+        bar.style.top = progress_config.top;
+        bar.style.width = progress_config.width;
+        bar.style.height = progress_config.height;
+        bar.style.background = progress_config.background;
+        var fill = document.createElement("div");
+        fill.style.width = "0%";
+        fill.style.height = "100%";
+        fill.style.top = "0";
+        fill.style.left = "0";
+        fill.style.background = progress_config.color;
+        bar.appendChild(fill);
+        game.progress = bar;
+        container.appendChild(bar);  
+    }
+    game.progress.childNodes[0].style.width = progress * 100 + "%";
     if (progress >= 1) {
-        setTimeout(function() {
-            progress_bar.bar.style.display = "none";
-        }, 150);
+        game.progress.style.display = "none";
     }
 };
 
@@ -93,25 +71,19 @@ App.prototype.startNativeApp = function() {
     var me = this;
     me.is_unity_ready = true;
     window.onbeforeunload = function() {
-        Unity.call(JSON.stringify({ action: "onGameEnd" }));
+        Unity.call(JSON.stringify({"action": "onGameEnd"}));
     };
-    Unity.call(JSON.stringify({
-        action: "onUnityWebviewResize",
-        top_bar_height: me.top_bar_height,
-    }));
+    Unity.call(JSON.stringify({"action": "onUnityWebviewResize",
+                               "top_bar_height": me.top_bar_height }));
     // forward WebView postMessage data from parent window
-    window.addEventListener("message", function (event) {
+    window.addEventListener('message', function (event) {
         if (event.data["action"] == "androidunity") {
             window.app.processUnityData(event.data["data_string"]);
         }
     });
     // tell webView screen.html is ready
     var parts = document.location.href.split("/");
-    Unity.call(JSON.stringify({
-        action: "onLaunchApp",
-        game_id: parts[parts.length-3].replace(".cdn.airconsole.com", ""),
-        game_version: parts[parts.length-2],
-    }));
+    Unity.call(JSON.stringify({"action": "onLaunchApp", "game_id" : parts[parts.length-3].replace(".cdn.airconsole.com", ""), "game_version" : parts[parts.length-2]}));
 };
 
 App.prototype.setupEditorSocket = function() {
@@ -133,8 +105,8 @@ App.prototype.setupEditorSocket = function() {
         document.body.innerHTML = "<div style='position:absolute; top:50%; left:0%; width:100%; margin-top:-32px; color:white;'><div style='font-size:32px'>Game <span style='color:red'>stopped</span> in Unity. Please close this tab.</div></div>";
     };
     document.body.innerHTML = "<div style='position:absolute; top:50%; left:0%; width:100%; margin-top:-32px; color:white;'>"
-      + "<div id='editor-message' style='text-align:center; font-family: Arial'><div style='font-size:32px;'>You can see the game scene in the Unity Editor.</div><br>Keep this window open in the background.</div>"
-      + "</div>";
+        + "<div id='editor-message' style='text-align:center; font-family: Arial'><div style='font-size:32px;'>You can see the game scene in the Unity Editor.</div><br>Keep this window open in the background.</div>"
+        + "</div>";
 };
 
 App.prototype.initAirConsole = function() {
@@ -170,41 +142,41 @@ App.prototype.initAirConsole = function() {
             "device_data": device_data
         });
     };
-
+    
     me.airconsole.onConnect = function (device_id) {
         me.postToUnity({
             "action": "onConnect",
             "device_id": device_id
         });
     };
-
+    
     me.airconsole.onDisconnect = function (device_id) {
         me.postToUnity({
             "action": "onDisconnect",
             "device_id": device_id
         });
     };
-
+    
     me.airconsole.onCustomDeviceStateChange = function (device_id) {
         me.postToUnity({
             "action": "onCustomDeviceStateChange",
             "device_id": device_id
         });
     };
-
+    
     me.airconsole.onDeviceProfileChange = function(device_id) {
         me.postToUnity({
             "action": "onDeviceProfileChange",
             "device_id": device_id
         });
     };
-
+    
     me.airconsole.onAdShow = function() {
         me.postToUnity({
             "action": "onAdShow"
         });
     };
-
+    
     me.airconsole.onAdComplete = function(ad_was_shown) {
         me.postToUnity({
             "action": "onAdComplete",
@@ -233,7 +205,7 @@ App.prototype.initAirConsole = function() {
         });
     };
 
-    me.airconsole.onPersistentDataLoaded = function(data) {
+        me.airconsole.onPersistentDataLoaded = function(data) {
         me.postToUnity({
             "action": "onPersistentDataLoaded",
             "data": data
@@ -252,15 +224,15 @@ App.prototype.initAirConsole = function() {
 App.prototype.getURLParameterByName = function(name) {
     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-      results = regex.exec(location.search);
+        results = regex.exec(location.search);
     return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 };
 
 App.prototype.setupErrorHandler = function() {
     window.onerror = function(message) {
         if (message.indexOf("UnknownError") != -1 ||
-          message.indexOf("Program terminated with exit(0)") != -1 ||
-          message.indexOf("DISABLE_EXCEPTION_CATCHING") != -1) {
+            message.indexOf("Program terminated with exit(0)") != -1 ||
+            message.indexOf("DISABLE_EXCEPTION_CATCHING") != -1) {
             // message already correct, but preserving order.
         } else if (message.indexOf("Cannot enlarge memory arrays") != -1) {
             window.setTimeout(function() {
@@ -268,8 +240,8 @@ App.prototype.setupErrorHandler = function() {
             });
             return false;
         } else if (message.indexOf("Invalid array buffer length") != -1 ||
-          message.indexOf("out of memory") != -1 ||
-          message.indexOf("Array buffer allocation failed") != -1) {
+            message.indexOf("out of memory") != -1 ||
+            message.indexOf("Array buffer allocation failed") != -1) {
             alert("Your browser ran out of memory. Try restarting your browser and close other applications running on your computer.");
             return true;
         }
@@ -303,7 +275,7 @@ App.prototype.setupErrorHandler = function() {
 App.prototype.postQueue = function () {
     if (this.queue !== false) {
         for (var i = 0; i < this.queue.length; ++i) {
-            this.postToUnity(this.queue[i]);
+        this.postToUnity(this.queue[i]);
         }
         this.queue = false;
     }
@@ -311,24 +283,24 @@ App.prototype.postQueue = function () {
 
 App.prototype.postToUnity = function (data) {
     if (this.is_unity_ready) {
-        if (this.is_editor) {
-            // send data over websocket
-            this.unity_socket.send(JSON.stringify(data));
-        } else if (this.is_native_app) {
+	    if (this.is_editor) {
+	        // send data over websocket
+	        this.unity_socket.send(JSON.stringify(data));
+	    } else if (this.is_native_app) {
             // send data over webview interface
-            Unity.call(JSON.stringify(data));
-        } else {
-            // send data with SendMessage from Unity js library
-            this.game.SendMessage("AirConsole", "ProcessJS", JSON.stringify(data));
-        }
-    } else {
-        if (this.queue === false && data.action == "onReady") {
-            this.queue = [];
-        }
-        if (this.queue !== false) {
-            this.queue.push(data);
-        }
-    }
+	        Unity.call(JSON.stringify(data));
+	    } else {
+	        // send data with SendMessage from Unity js library
+	        this.game.SendMessage("AirConsole", "ProcessJS", JSON.stringify(data));
+	    }
+	} else {
+	    if (this.queue === false && data.action == "onReady") {
+		  this.queue = [];
+		}
+		if (this.queue !== false) {
+		  this.queue.push(data);
+		}
+	}
 };
 
 App.prototype.processUnityData = function (data) {
@@ -345,8 +317,8 @@ App.prototype.processUnityData = function (data) {
     } else if (data.action == "showDefaultUI") {
         this.airconsole.showDefaultUI(data.data);
         if (this.is_native_app) {
-            Unity.call(JSON.stringify({"action": "onUnityWebviewResize",
-                "top_bar_height": data.data ? this.top_bar_height : 0}));
+          Unity.call(JSON.stringify({"action": "onUnityWebviewResize",
+                                     "top_bar_height": data.data ? this.top_bar_height : 0}));
         }
     } else if (data.action == "navigateHome") {
         this.airconsole.navigateHome();
@@ -370,30 +342,30 @@ App.prototype.processUnityData = function (data) {
 };
 
 App.prototype.resizeCanvas = function() {
-    var aspect_ratio = this.web_config.width / this.web_config.height;
+    var container = this.game.container;
+    var aspectRatio = this.web_config.width / this.web_config.height;
     var w, h;
 
-    if (window.innerWidth/aspect_ratio > window.innerHeight) {
-        w = window.innerHeight * aspect_ratio;
+    if (window.innerWidth/aspectRatio > window.innerHeight) {
+        w = window.innerHeight * aspectRatio;
         h = window.innerHeight;
     } else {
         w = window.innerWidth;
-        h = window.innerWidth / aspect_ratio;
+        h = window.innerWidth / aspectRatio;
     }
 
     // Setting canvas size
-    this.game_container.style.width = w + "px";
-    this.game_container.style.height = h + "px";
+    container.style.width = w + "px";
+    container.style.height = h + "px";
 };
 
 App.prototype.onGameReady = function(autoScale) {
     var me = this;
-
     me.is_unity_ready = true;
     me.postQueue();
 
     if (autoScale) {
-        window.addEventListener("resize", function() { me.resizeCanvas() });
+        window.addEventListener('resize', function() { me.resizeCanvas() });
         me.resizeCanvas();
     }
 };
